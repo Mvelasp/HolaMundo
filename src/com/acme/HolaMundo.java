7e6f2a868142873d4bc8ecc2cd3d04a9504d5ccf
Mvelasp <dlmichael007@gmial.com>
2021-11-25 18:09:32 -0500

    Se integro la clase mensaje

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -2,6 +2,11 @@ package com.acme;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+       // System.out.println("Hola Mundo!!");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribre tu nombre: ");
+        msg.saludar(nombre);
+
     }
+
 }
